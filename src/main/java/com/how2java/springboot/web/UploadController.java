821866bce86b7a69c72f83fd307b9326be93e326
CRIC-JK <405963004@qq.com>
2019-09-19 17:21:13 +0800

    SpringBoot 上传文件

---
+++ src/main/java/com/how2java/springboot/web/UploadController.java
@@ -0,0 +1,43 @@
+package com.how2java.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Controller
+public class UploadController {
+
+    @RequestMapping("/uploadPage")
+    public String uploadPage() {
+        return "uploadPage";
+    }
+
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+    public String upload(HttpServletRequest req, @RequestParam("file") MultipartFile file, Model m){
+        try {
+            String filename=System.currentTimeMillis()+file.getOriginalFilename();
+            String destFileName=req.getServletContext().getRealPath("")+"uploaded"+ File.separator+filename;
+            File destfile=new File(destFileName);
+            destfile.getParentFile().mkdir();
+            file.transferTo(destfile);
+            m.addAttribute("fileName",filename);
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "上传失败," + e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "上传失败," + e.getMessage();
+        }
+
+        return "showImg";
+    }
+}
