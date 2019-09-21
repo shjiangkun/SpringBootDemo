edf65a81879ddb18422d6969b65b45d84862a3af
shjiangkun <405963004@qq.com>
2019-09-21 11:18:51 +0800

    Mapper 要加@Repository注解

--- src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
@@ -3,9 +3,11 @@ package com.how2java.springboot.mapper;
 import com.how2java.springboot.pojo.Category;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 @Mapper
 public interface CategoryMapper {
 
